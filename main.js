b9584259db2add8db88aab6cf5033087dbfeb4e3
Joselyn <joselyneb27@gmail.com>
2025-01-22 19:10:24 -0400

    feat: add countries page

---
+++ main.js
@@ -0,0 +1,36 @@
+console.log('Paises1')
+        const Pag=fetch('https://restcountries.com/v3.1/independent?status=true')
+    /*    Pag.then(function(res){
+            console.log("3",res)
+            const res2= res.json()
+            console.log('res2 4',res2)
+            res2.then(function(caracteres){
+                console.log('caracteres de los paises  6',caracteres)
+            })
+            console.log(5)
+        })   */ 
+
+        Pag
+        .then(function(res){
+            return res.json()
+        })
+        .then(function(countries){
+            console.log('Countries',countries)
+
+            countries.forEach(country => {
+                const container=document.createElement('div')
+                const name=document.createElement('h2')
+                name.textContent=country.name.official
+                const population=document.createElement('p')
+                population.textContent= `Population: ${parseFloat(country.population).toLocaleString("en")}`
+                const image=document.createElement('img')
+                image.src=country.flags.png   
+                
+                container.appendChild(name)
+                container.appendChild(population)
+                container.appendChild(image)
+
+                document.body.appendChild(container)
+            });
+
+        })
